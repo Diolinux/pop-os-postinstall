e569e27c8d55ee38508ca301f3847de8bbe76780
vinirafaz <vinirafa0306@gmail.com>
2022-11-23 19:42:17 -0300

    dsadas

--- pop-os-postinstall.sh
+++ pop-os-postinstall.sh
@@ -16,15 +16,10 @@ set -e
 ##URLS
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb?source=website"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.2.50318-impish_amd64.deb"
-URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.0.3-12689/Ubuntu/Installer/x86_64/synology-drive-client-12689.x86_64.deb"
-
 
 ##DIRETÓRIOS E ARQUIVOS
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
-FILE="/home/$USER/.config/gtk-3.0/bookmarks"
 
 
 #CORES
@@ -76,24 +71,11 @@ sudo apt update -y
 ##DEB SOFTWARES TO INSTALL
 
 PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  winff
   virtualbox
-  ratbagd
-  gparted
-  timeshift
-  gufw
-  synaptic
-  solaar
-  vlc
   code
-  gnome-sushi 
-  folder-color
+  tcpdump
   git
   wget
-  ubuntu-restricted-extras
-  v4l2loopback-utils
- 
 )
 
 # ---------------------------------------------------------------------- #
@@ -131,18 +113,9 @@ install_flatpaks(){
 
   echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
 
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub org.telegram.desktop -y
-flatpak install flathub org.freedesktop.Piper -y
-flatpak install flathub org.chromium.Chromium -y
-flatpak install flathub org.gnome.Boxes -y
-flatpak install flathub org.onlyoffice.desktopeditors -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-flatpak install flathub org.flameshot.Flameshot -y
-flatpak install flathub org.electrum.electrum -y
+
 }
 
 ## Instalando pacotes Snap ##
@@ -168,37 +141,8 @@ apt_update -y
 flatpak update -y
 sudo apt autoclean -y
 sudo apt autoremove -y
-nautilus -q
 }
 
-
-# -------------------------------------------------------------------------- #
-# ----------------------------- CONFIGS EXTRAS ----------------------------- #
-
-#Cria pastas para produtividade no nautilus
-extra_config(){
-
-
-mkdir /home/$USER/TEMP
-mkdir /home/$USER/EDITAR 
-mkdir /home/$USER/Resolve
-mkdir /home/$USER/AppImage
-mkdir /home/$USER/Vídeos/'OBS Rec'
-
-#Adiciona atalhos ao Nautilus
-
-if test -f "$FILE"; then
-    echo "$FILE já existe"
-else
-    echo "$FILE não existe, criando..."
-    touch /home/$USER/.config/gkt-3.0/bookmarks
-fi
-
-echo "file:///home/$USER/EDITAR 🔵 EDITAR" >> $FILE
-echo "file:///home/$USER/AppImage" >> $FILE
-echo "file:///home/$USER/Resolve 🔴 Resolve" >> $FILE
-echo "file:///home/$USER/TEMP 🕖 TEMP" >> $FILE
-}
 
 # -------------------------------------------------------------------------------- #
 # -------------------------------EXECUÇÃO----------------------------------------- #
@@ -213,7 +157,6 @@ just_apt_update
 install_debs
 install_flatpaks
 install_snaps
-extra_config
 apt_update
 system_clean
 
